0d08ff5532537e0315a23583eb950c776fdabe0c
Dvorkin Dmitry <dvorkin@tibbo.com>
2023-05-25 21:00:17 +0300

    GPIO is persistent to make it work well with NodeJS GC

--- modules/led.js
+++ modules/led.js
@@ -1,25 +1,25 @@
 const TPSpmap = require( '@tibbo-tps/pinmap');
 
-//const { version, Chip, Line } = require( "node-libgpiod");
-//
-//const chip = new Chip(0);
-//console.log( 'Using pin#' + TPSpmap.getRpin( LED_BLU) + ' (' + LED_BLU + ') as BLUE');
-//var blu = new Line(chip, TPSpmap.getRpin( LED_BLU));
-//console.log( 'Using pin#' + TPSpmap.getRpin( LED_RED) + ' (' + LED_RED + ') as RED');
-//var red = new Line(chip, TPSpmap.getRpin( LED_RED));
-//
-//blu.requestOutputMode();
-//red.requestOutputMode();
-//
-//blu.setValue( 1);
-//red.setValue( 1);
-//
+const { version, Chip, Line } = require( "node-libgpiod");
+
+global.chip = new Chip(0);
+console.log( 'Using pin#' + TPSpmap.getRpin( LED_BLU) + ' (' + LED_BLU + ') as BLUE');
+global.blu = new Line(chip, TPSpmap.getRpin( LED_BLU));
+console.log( 'Using pin#' + TPSpmap.getRpin( LED_RED) + ' (' + LED_RED + ') as RED');
+global.red = new Line(chip, TPSpmap.getRpin( LED_RED));
+
+blu.requestOutputMode();
+red.requestOutputMode();
+
+blu.setValue( 1);
+red.setValue( 1);
+
 exports.blink = function(color){
-//    if(color === "red"){
-////        red.setValue(0);
-////        setTimeout(function() {  red.setValue(1);  },500)
-//    }else if (color === "blue"){
-////        blu.setValue(0);
-////        setTimeout(function(){  blu.setValue(1);  },500)
-//    }
+    if(color === "red"){
+        red.setValue(0);
+        setTimeout(function() {  red.setValue(1);  },500)
+    }else if (color === "blue"){
+        blu.setValue(0);
+        setTimeout(function(){  blu.setValue(1);  },500)
+    }
 };
